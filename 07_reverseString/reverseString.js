4d1e717b60756a5c24a56a63d93d8147f4df630a
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2025-11-10 11:31:08 -0700

    Add solution to javascript exercises
    This is my solution to the exercises updated to GitHub.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+// const reverseString = function(str) {
+//     let breakDown = str.split('');
+//     let result = '';
+//     for (let i = breakDown.length - 1; i >= 0; i--){
+//         console.log(breakDown[i]);
+//         result += breakDown[i];
+//     }
+//     console.log(breakDown.length);
+//     console.log(breakDown);
+//     console.log(result);
+// };
 
-};
+// reverseString('String');
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
+}
 
+// console.log(reverseString("String"));
 // Do not edit below this line
 module.exports = reverseString;
