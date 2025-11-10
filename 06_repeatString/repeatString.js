89bb3494b10e108d27d1ac475a5960aca1c60881
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2025-11-10 11:35:37 -0700

    Add my solution to the JavaScript exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, mult) {
+    if (mult < 0) return "ERROR";
+    let string = "";
+    for (let x = 0; x < mult; x++) {
+        string += str;
+    }
+    return string;
 };
+let str = 'This is a string.'
+console.log(str.repeat(5));
 
 // Do not edit below this line
 module.exports = repeatString;
