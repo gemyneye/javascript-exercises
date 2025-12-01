f74f6782ff8f0cb76789b60e06647af793f0283e
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2025-12-01 13:56:33 -0700

    Add code for solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,20 +1,14 @@
-const reverseString = function(str) {
-    let breakDown = str.split('');
-    let result = '';
-    for (let i = breakDown.length - 1; i >= 0; i--){
-        console.log(breakDown[i]);
-        result += breakDown[i];
-    }
-    console.log(breakDown.length);
-    console.log(breakDown);
-    console.log(result);
-};
+function reverseString(str) {
+    return str.split('')
+    .reverse()
+    .join('');
+}
+console.log(reverseString('string'));
 
 // reverseString('String');
-const reverseString = function(str) {
-    return str.split('').reverse().join('');
-}
+//const reverseString = function(str) {
+//    return str.split('').reverse().join('');
+//}
 
-// console.log(reverseString("String"));
 // Do not edit below this line
 module.exports = reverseString;
