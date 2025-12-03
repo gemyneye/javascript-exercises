48fbd0393a269847d31a6657f865af15c9915c67
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2025-12-03 11:50:14 -0700

    Added solution with arrow function syntax

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,19 +1,21 @@
-const removeFromArray = function(arr, ...args) {
-    for (const arg of args) {
-        //console.log(arg);
-        let i = 0;
-        while (i < arr.length){
-            if (arr[i] === arg){
-                arr.splice(i,1,);
-            } else {
-            i++;
-            }
-        }
-    }
-    return arr;
-};
-// let myArr = [1, 5, 2, 3, 4, 5, 5, 7, 5];
-// console.log(removeFromArray(myArr, 7, 3, 5));
+const removeFromArray = (arr, ...args) => arr.filter(x => !args.includes(x));
+    // for (const arg of args) {
+    //     //console.log(arg);
+    //     let i = 0;
+    //     while (i < arr.length){
+    //         if (arr[i] === arg){
+    //             arr.splice(i,1,);
+    //         } else {
+    //         i++;
+    //         }
+    //     }
+    // }
+    // return arr;
+    // const myNewArr = arr.filter(x => !args.includes(x));
+    // return myNewArr;
+
+let myArr = [1, 5, 2, 3, 4, 5, 5, 7, 5];
+console.log(removeFromArray(myArr, 7, 3, 5));
 
 
 // Do not edit below this line
