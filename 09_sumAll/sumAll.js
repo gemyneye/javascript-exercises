6671c6af460fd5b56d5c139d8b7a808d1558c14b
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2025-12-03 15:08:25 -0700

    Clean up logic and order

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -14,18 +14,14 @@ function addEmUp(smallNumber, bigNumber) {
     return start;
 }
 const sumAll = function(start, end) {
-    if (start > end) {
-        let smallNumber = end;
-        console.log(end);
-        let bigNumber = start;
-        console.log(start);
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR';
+    }  else if (start < 0 || end < 0) {
+        return 'ERROR';
+    }  else if (start > end) {
+        const smallNumber = end;
+        const bigNumber = start;
         return addEmUp(smallNumber, bigNumber);
-    } else if (start < 0 || end < 0) {
-        return 'ERROR';
-    } else if ((typeof start !== "number") || (typeof end !== "number")) {
-        return "ERROR";
-    } else if (!Number.isInteger(start) || !Number.isInteger(end)) {
-        return 'ERROR';
     } else {
         return addEmUp(start, end);
     }   
