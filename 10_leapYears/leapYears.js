76c30ffb2289d5a76748c166bf4f386e3318ef9a
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2025-12-05 05:37:35 -0700

    Add first solution

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) {
+        return "ERROR"
+    } else if ((year % 100 == 0) && (year % 400 !== 0)) {
+        return "ERROR";
+    } else if (year % 4 !== 0) {
+        return "ERROR";
+    } else {
+        return true //`${year} is a leap year.`
+    }
 };
+
+console.log(leapYears(2008));
 
 // Do not edit below this line
 module.exports = leapYears;
