dd1db3fb25d03ed1892d2fd5ecefff72e626a1ec
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2025-12-05 05:39:27 -0700

    Add result to meet exercise expectation

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,10 +1,10 @@
 const leapYears = function(year) {
     if (!Number.isInteger(year)) {
-        return "ERROR"
+        return false;
     } else if ((year % 100 == 0) && (year % 400 !== 0)) {
-        return "ERROR";
+        return false;
     } else if (year % 4 !== 0) {
-        return "ERROR";
+        return false;
     } else {
         return true //`${year} is a leap year.`
     }
